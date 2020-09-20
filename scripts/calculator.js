75a7f9cf14c89e437ec10a970c7aad0a9932d793
emilybonar <emilykbonar@gmail.com>
2020-09-20 09:48:18 -0500

    Improved code

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,24 +1,23 @@
 const inputs = document.querySelectorAll("input");
-const outputs = document.querySelectorAll(".out");
+const outputs = document.querySelectorAll("output");
 
-inputs.forEach((input) =>
-	input.addEventListener("input", (e) => {
-		calc();
-	}),
-);
+calc();
+
+const form = document.querySelector("form");
+form.addEventListener("input", (e) => {
+	calc();
+});
 
 function calc() {
 	const hydration = [inputs[0].value / 100, inputs[1].value / 100];
 	const masses = [inputs[2].value, inputs[3].value, inputs[4].value];
 
 	outputs[0].innerHTML = Math.round(
-		masses[0] / (hydration[0] + 1) +
-			(hydration[1] * masses[0]) / (hydration[0] + 1),
+		(masses[0] / (hydration[0] + 1)) * (hydration[1] + 1),
 	);
 	outputs[1].innerHTML = masses[1];
 	outputs[2].innerHTML = Math.round(
 		masses[2] -
-			((hydration[1] * masses[0]) / (hydration[0] + 1) -
-				(hydration[0] * masses[0]) / (hydration[0] + 1)),
+			((hydration[1] - hydration[0]) * masses[0]) / (hydration[0] + 1),
 	);
 }
